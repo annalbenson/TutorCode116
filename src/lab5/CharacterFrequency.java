89527a78775bc8fb9c107737ec9143519044b4b6
annalbenson <annalucybenson@gmail.com>
2018-10-11 15:40:37 -0500

    lab 5 test files added

--- src/lab5/CharacterFrequency.java
+++ src/lab5/CharacterFrequency.java
@@ -3,8 +3,8 @@ package lab5;
 public class CharacterFrequency {
 
     public static final int MAX_SIZE = 100;
-    Data [] frequencyTable;
-    int count;
+    private Data [] frequencyTable;
+    private int count;
 
     public CharacterFrequency(){
         frequencyTable = new Data [MAX_SIZE];
@@ -12,6 +12,29 @@ public class CharacterFrequency {
     }
 
     public void counter(String input){
+
+        boolean found;
+        char c, symbol;
+        for(int i = 0; i < input.length(); i++ ){
+            // go through input character by character
+            c = input.charAt(i);
+            found = false;
+            for(int j = 0; j < count; j++){
+                //determine whether character c already has a Data object
+                symbol = frequencyTable[j].getSymbol();
+                if(c == symbol){
+                    // c does have an entry, increment count
+                    frequencyTable[j].incrementCount();
+                    found = true;
+                    break; // avoid extra looping
+                }
+            }
+            if( ! found ){
+                // create new data object
+                frequencyTable[count] = new Data(c);
+                count++;
+            }
+        }
 
     }
 
